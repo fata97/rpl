5da520bb1cba0f0e9abf6b245289d27854d7c2be
fata97 <fatahasan97@gmail.com>
2018-01-30 17:35:30 +0700

    push

--- table_guru.php
+++ table_guru.php
@@ -7,13 +7,35 @@
 	<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
         <thead>
             <tr>
+								<th>NO</th>
                 <th>NIP</th>
                 <th>NAMA</th>
                 <th>ALAMAT</th>
                 <th>NO_HP</th>
+								<th>AKSI</th>
             </tr>
         </thead>
         <tbody>
+					<?php
+			        include "include/koneksi.php";
+			        $i = 0 + 1;
+			        $sql = mysqli_query($conn, "SELECT * FROM guru ORDER BY nama");
+			        while ($hasil = mysqli_fetch_array($sql)) {
+			     ?>
+					 <tr>
+			      <td style="text-align: center;"><?php echo $i; ?></td>
+			      <td><?php echo $hasil['nip']; ?></td>
+			      <td><?php echo $hasil['nama']; ?></td>
+						<td><?php echo $hasil['alamat']; ?></td>
+						<td><?php echo $hasil['no_hp']; ?></td>
+			      <td style="text-align: center;"><a href="editguru.php?edit=<?php echo $hasil['nip']; ?>" class="btn btn-warning">Edit</a>
+			      <a href="proses-hapus-guru.php?hapus=<?php echo $hasil['nip']; ?>" class="btn btn-danger">Hapus</a></td>
+			  </tr>
+			  <?php
+			      $i++;
+			      }
+			    ?>
+
         </tbody>
     </table>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
