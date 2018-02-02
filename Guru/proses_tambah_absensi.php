7fc64c6295840d0da591a228135bb752baacc738
ozyexplorer <ozisambadha@gmail.com>
2018-02-02 11:59:25 +0700

    a

--- Guru/proses_tambah_absensi.php
+++ Guru/proses_tambah_absensi.php
@@ -2,17 +2,42 @@
 include "../include/koneksi.php";
 
 $tanggal = $_POST["tanggal"];
-$hadir = $_POST["hadir"];
-$sakit = $_POST["sakit"];
-$izin = $_POST["izin"];
-$alfa = $_POST["alfa"];
-$kehadiran = $_POST["kehadiran"];
+
+if(empty($_POST["hadir"])){
+	$hadir[] = 0;
+}else{
+	$hadir[] = 1;
+}
+
+
+if(empty($_POST["sakit"])){
+	$sakit[] = 0;
+}else{
+	$sakit[] = 1;
+}
+
+if(empty($_POST["izin"])){
+	$izin[] = 0;
+}else{
+	$izin[] = 1;
+}
+
+if(empty($_POST["alfa"])){
+	$alfa[] = 0;
+}else{
+	$alfa[] = 1;
+}
+// $hadir = $_POST["hadir"];
+// $sakit = $_POST["sakit"];
+// $izin = $_POST["izin"];
+// $alfa = $_POST["alfa"];
+// $kehadiran = $_POST["kehadiran"];
 $nis = $_POST["nis"];
 $id_mapel = $_POST["id_mapel"];
-$jumlah_dipilih = count($tanggal);
+$jumlah_dipilih = count($nis);
 
 // if ($kehadiran == "hadir") {
-//     $hadir = 1;
+//     $hadir[] = 1;
 //     $alfa = 0;
 //     $izin = 0;
 //     $sakit = 0;
@@ -38,20 +63,22 @@ $jumlah_dipilih = count($tanggal);
 //   $sakit = 0;
 // }
 
-
+// for($x=0;$x<$jumlah_dipilih;$x++){
+// echo $hadir[$x];
+// }
 
 // if(empty($_POST["tanggal"]) || empty($_POST["kehadiran"]) || empty($_POST["nis"]) || empty($_POST["id_mapel"])){
 // 	echo "<script language='javascript'>alert('Gagal di tambahkan');</script>";
 // 	echo '<meta http-equiv="refresh" content="0; url=data_absensi.php">';
 // }else{
 for($x=0;$x<$jumlah_dipilih;$x++){
-  mysqli_query($conn, "INSERT INTO `absensi`(`id_absensi`, `tanggal`, `hadir`, `alfa`, `izin`, `sakit`, `nis`, `id_mapel`)
-			VALUES (NULL, '$tanggal[$x]', '$hadir[$x]', '$alfa[$x]', '$izin[$x]', '$sakit[$x]', '$nis[$x]', '$id_mapel[$x]')");
+ mysqli_query($conn, "INSERT INTO `absensi`(`id_absensi`, `tanggal`, `hadir`, `alfa`, `izin`, `sakit`, `nis`, `id_mapel`)
+		VALUES (NULL, '$tanggal[$x]', '$hadir[$x]', '$alfa[$x]', '$izin[$x]', '$sakit[$x]', '$nis[$x]', '$id_mapel[$x]')");
 	// mysql_query("INSERT INTO makanan values('','$makanan')");
 }
 	// $sql = "";
 	// 		$kueri =
-			echo "<script language='javascript'>alert('Berhasil Absen');</script>";
-    	echo '<meta http-equiv="refresh" content="0; url=data_absensi.php">';
+ echo "<script language='javascript'>alert('Berhasil Absen');</script>";
+  	echo '<meta http-equiv="refresh" content="0; url=data_absensi.php">';
 
 ?>
